d1d9312fc104b7047089c28a4d942068a0ad5c6e
Prince Sanguan <princesanguan44@gmail.com>
2024-01-13 23:01:28 +0800

    Commiting this project

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html class="no-js"> 
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Carol and Prince Wedding</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<link href='https://fonts.googleapis.com/css?family=Work+Sans:400,300,600,400italic,700' rel='stylesheet' type='text/css'>
+	<link href="https://fonts.googleapis.com/css?family=Sacramento" rel="stylesheet">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="{{ asset('css/animate.css') }}">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="{{ asset('css/icomoon.css') }}">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="{{ asset('css/magnific-popup.css') }}">
+
+	<!-- Owl Carousel  -->
+	<link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}">
+	<link rel="stylesheet" href="{{ asset('css/owl.theme.default.min.css') }}">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+	<!-- Modernizr JS -->
+	<script src="{{ asset('js/modernizr-2.6.2.min.js') }}"></script>
+
+	<script src="{{ asset('js/respond.min.js') }}"></script>
+
+
+	</head>
+	<body>
+		
+	<div class="fh5co-loader"></div>
+	
+	<div id="page">
+    <nav class="fh5co-nav text-center" role="navigation">
+      <div class="container">
+          <div class="row">
+              <div class="col-xs-12">
+                  <div id="fh5co-logo">
+                      <a href="index.html">Invitation<strong></strong></a>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </nav>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,294 @@
+@include('includes.header')
+	<header id="fh5co-header" class="fh5co-cover" role="banner" style="background-image:url({{ asset('images/carousel-1.jpg') }}); background-size: cover; background-position: center center;" data-stellar-background-ratio="0.5">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center">
+					<div class="display-t">
+						<div class="display-tc animate-box" data-animate-effect="fadeIn">
+							<h1>Prince &amp; Carol</h1>
+							<h2>We Are Getting Married</h2>
+							<div class="simply-countdown simply-countdown-one"></div>
+							<p><a href="#fh5co-started" class="btn btn-default btn-sm">Punta ka?</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<div id="fh5co-couple">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box">
+					<h2>Hello!</h2>
+					<h3>January 27th, 2024 Silang, Cavite</h3>
+					<p>We invited you to celebrate our wedding</p>
+				</div>
+			</div>
+			<div class="couple-wrap animate-box">
+				<div class="couple-half">
+					<div class="groom">
+						<img src="{{ asset ('images/prince.jpg') }}" alt="groom" class="img-responsive">
+					</div>
+					<div class="desc-groom">
+						<h3>Prince E. Sanguan</h3>
+						<p>Magandang araw! Ako si Prince, at magpapakasal ako ngayong January 27. Iniimbitahan ko kayo sa aming espesyal na araw para makiisa sa pagdiriwang ng pagmamahalan namin. Maraming salamat!</p>
+					</div>
+				</div>
+				<p class="heart text-center"><i class="icon-heart2"></i></p>
+				<div class="couple-half">
+					<div class="bride">
+						<img src="{{ asset('images/carol.jpg')}}" alt="groom" class="img-responsive">
+					</div>
+					<div class="desc-bride">
+						<h3>Carol Corpuz</h3>
+						<p>Good day everyone! Ako po si Carol Corpuz, ako nga pala ang kinababaliwan este ang mapapangasawa ni Prince Sanguan. Kami ay masaya na kayo ay makakapunta sa special day ng aming buhay, panalangin ko na maging maayos ang araw na iyon na gaganapin sa araw ng Enero 27,2024. Thank you! Stay safe and enjoy ❤️</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="fh5co-event" class="fh5co-bg" style="background-image:url({{ asset('images/wedding.jpg') }});">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box">
+					<span>Our Special Events</span>
+					<h2>Batino's Farm Resort</h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="display-t">
+					<div class="display-tc">
+						<div class="col-md-10 col-md-offset-1">
+							<div class="col-md-6 col-sm-6 text-center">
+								<div class="event-wrap animate-box">
+									<h3>Main Event</h3>
+									<div class="event-col">
+										<i class="icon-clock"></i>
+										<span>5:00 PM</span>
+										<span>6:00 PM</span>
+									</div>
+									<div class="event-col">
+										<i class="icon-calendar"></i>
+										<span>Saturday, 28</span>
+										<span>January, 2024</span>
+									</div>
+									<p>Simpleng kasiyahan na may kunting pagdiriwang na gaganapin dito. San naway makapunta at makadalo kayo. Salamat ng Marami.</p>
+								</div>
+							</div>
+							<div class="col-md-6 col-sm-6 text-center">
+								<div class="event-wrap animate-box">
+									<h3>Wedding Ceremony</h3>
+									<div class="event-col">
+										<i class="icon-clock"></i>
+										<span>7:00 PM</span>
+										<span>12:00 PM</span>
+									</div>
+									<div class="event-col">
+										<i class="icon-calendar"></i>
+										<span>Sunday 28</span>
+										<span>January, 2024</span>
+									</div>
+									<p>Kunting kasiyahan na may kunting swimmingan at kainan. Kaya sana magenjoy kayong lahat para lahat tayo happy go lucky!</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="fh5co-testimonial">
+		<div class="container">
+			<div class="row">
+				<div class="row animate-box">
+					<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+						<span>Best Wishes</span>
+						<h2>Friends Wishes</h2>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12 animate-box">
+						<div class="wrap-testimony">
+							<div class="owl-carousel-fullwidth">
+								<div class="item">
+									<div class="testimony-slide active text-center">
+										<figure>
+											<img src="{{ asset('images/jeffranie.jpg') }}" alt="user">
+										</figure>
+										<span>Jeffranie Etulle, via <a href="#" class="twitter">Facebook</a></span>
+										<blockquote>
+											<p>"Ang swerte ni carol kasi napakabait ni kuya rerenz. Isa syang ideal man kumbaga sa lotto nanalo sya!"</p>
+										</blockquote>
+									</div>
+								</div>
+								<div class="item">
+									<div class="testimony-slide active text-center">
+										<figure>
+											<img src="images/johnpaul.jpg" alt="user">
+										</figure>
+										<span>John Paul Aquino, via <a href="#" class="twitter">Facebook</a></span>
+										<blockquote>
+											<p>"Kumbaga sa ginto napaka rare ni Prince. Swerte ni Carol at sya ang pinili. Dapat lagi syang magpasalamat sa panginoon"</p>
+										</blockquote>
+									</div>
+								</div>
+								<div class="item">
+									<div class="testimony-slide active text-center">
+										<figure>
+											<img src="images/marvin.jpg" alt="user">
+										</figure>
+										<span>Marvin Corpuz, via <a href="#" class="twitter">Messenger</a></span>
+										<blockquote>
+											<p>"Dati ako ang inuutangan ni Carol, ngayon sya na inuutangan ko."</p>
+										</blockquote>
+									</div>
+								</div>
+
+								<div class="item">
+									<div class="testimony-slide active text-center">
+										<figure>
+											<img src="images/angelica.jpg" alt="user">
+										</figure>
+										<span>Angelica Tiongson, via <a href="#" class="twitter">Instagram</a></span>
+										<blockquote>
+											<p>"Congrats Both Side! Best Wishes!!"</p>
+										</blockquote>
+									</div>
+								</div>
+
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="fh5co-started" class="fh5co-bg" style="background-image:url(images/weddings.jpg);">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<h2>Pupunta ka ba?</h2>
+					<p>Please Fill-up the form para makasama ka sa swimming. Thanks.</p>
+				</div>
+			</div>
+			<div class="row animate-box">
+				<div class="col-md-10 col-md-offset-1">
+					<form class="form-inline">
+						<div class="col-md-4 col-sm-4">
+							<div class="form-group">
+								<label for="name" class="sr-only">Name</label>
+								<input type="name" class="form-control" name="name" placeholder="Buong Pangalan mo?" required>
+							</div>
+						</div>
+						<div class="col-md-4 col-sm-4">
+							<div class="form-group">
+								<label for="email" class="sr-only">Email</label>
+								<input type="email" class="form-control" id="email" placeholder="Email Address mo?" required>
+							</div>
+						</div>
+						<div class="col-md-4 col-sm-4">
+							<input type="submit" value="Pupunta ako" class="btn btn-default btn-block">
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<footer id="fh5co-footer" role="contentinfo">
+		<div class="container">
+
+			<div class="row copyright">
+				<div class="col-md-12 text-center">
+					<p>
+						<small class="block">&copy; 2024 All Rights Reserved.</small> 
+						<small class="block">Designed by <a href="https://prince-sanguan.vercel.app/" target="_blank">Prince E. Sanguan</small>
+					</p>
+					<p>
+						<ul class="fh5co-social-icons">
+                            <li><a href="https://www.instagram.com/princesanguan/"><i class="icon-instagram"></i></a></li>
+							<li><a href="https://www.facebook.com/Grandmaster1997/"><i class="icon-facebook"></i></a></li>
+							<li><a href="https://www.linkedin.com/in/prince-sanguan-a78988274/"><i class="icon-linkedin"></i></a></li>
+							<li><a href="https://github.com/PrinceSanguan"><i class="icon-github"></i></a></li>
+						</ul>
+					</p>
+				</div>
+			</div>
+
+		</div>
+	</footer>
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
+	
+	<!-- jQuery -->
+	<script src="{{ asset('js/jquery.min.js') }}"></script>
+	<!-- jQuery Easing -->
+	<script src="{{ asset('js/jquery.easing.1.3.js') }}"></script>
+	<!-- Bootstrap -->
+	<script src="{{ asset('js/bootstrap.min.js') }}"></script>
+	<!-- Waypoints -->
+	<script src="{{ asset('js/jquery.waypoints.min.js') }}"></script>
+	<!-- Carousel -->
+	<script src="{{ asset('js/owl.carousel.min.js') }}"></script>
+	<!-- countTo -->
+	<script src="{{ asset('js/jquery.countTo.js') }}"></script>
+
+	<!-- Stellar -->
+	<script src="{{ asset('js/jquery.stellar.min.js') }}"></script>
+	<!-- Magnific Popup -->
+	<script src="{{ asset('js/jquery.magnific-popup.min.js') }}"></script>
+	<script src="{{ asset('js/magnific-popup-options.js') }}"></script>
+
+	<<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/0.0.1/prism.min.js"></script>
+	<script src="{{ asset('js/simplyCountdown.js') }}"></script>
+	<!-- Main -->
+	<script src="{{ asset('js/main.js') }}"></script>
+
+	<script>
+
+var endDate = new Date("January 27, 2024 17:00:00");
+
+simplyCountdown('.simply-countdown-one', {
+    year: endDate.getFullYear(),
+    month: endDate.getMonth() + 1,
+    day: endDate.getDate(),
+    hours: endDate.getHours(),
+    minutes: endDate.getMinutes(),
+    seconds: endDate.getSeconds(),
+    words: {
+        days: 'Day',
+        hours: 'Hour',
+        minutes: 'Minute',
+        seconds: 'Second',
+        pluralLetter: 's'
+    },
+    plural: true,
+    inline: false,
+    inlineClass: 'simply-countdown-inline',
+    alwaysTwoDigits: false,
+    inlineCompact: false,
+    onEnd: function () {
+        console.log('Time has run out!');
+    },
+    refresh: 1000,
+    sectionClass: 'simply-section',
+    amountClass: 'simply-amount',
+    wordClass: 'simply-word',
+    zeroPad: false,
+    countUp: false
+});
+
+</script>
+
+	</body>
+</html>
